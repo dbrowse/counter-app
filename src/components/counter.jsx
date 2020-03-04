baa794d34019f2baf7b7371516662688fe94f890
dgeorgiev <dgeorgiev_job@yahoo.com>
2020-03-04 21:48:13 +0200

    First commit some component for counter class

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+	state = {};
+	render() {
+		return (
+			<div>
+				<h1>hello world</h1>
+				<button>Add</button>
+			</div>
+		);
+	}
+}
+
+export default Counter;
