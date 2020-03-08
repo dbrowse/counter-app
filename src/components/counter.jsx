f61ec0bd99a4c342048a743227318b49f365ce58
dgeorgiev <dgeorgiev_job@yahoo.com>
2020-03-08 21:31:14 +0200

    Passing children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,45 +3,29 @@ import React, { Component } from "react";
 class Counter extends Component {
 	//special property
 	state = {
-		count: 0,
-		tags: ["tag1", "tag2", "tag3"]
+		count: this.props.value
 	};
 
-	// constructor() {
-	// 	super();
-	// 	//functions are object and we have bind to set the value of 'this' to return new instcance
-	// 	//and reference current object
-	// 	this.handleIncrement = this.handleIncrement.bind(this);
-	// }
-
-	handleIncrement = () => {
-		console.log("Button clicked", this);
+	handleIncrement = product => {
+		console.log(product);
 		this.setState({ count: this.state.count + 1 });
 	};
 
-	renderTags() {
-		if (this.state.tags.length === 0) return <p>No more tags</p>;
-
-		return (
-			<ul>
-				{this.state.tags.map(tag => (
-					<li key={tag}>{tag}</li>
-				))}
-			</ul>
-		);
-	}
+	doHandleIncrement = () => {
+		this.handleIncrement({ id: 1 });
+	};
 
 	render() {
+		console.log("props", this.props);
 		return (
 			<div>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button
-					onClick={this.handleIncrement}
+					onClick={this.doHandleIncrement}
 					className='btn btn-secondary btn-sm'
 				>
 					Increment
 				</button>
-				{this.renderTags()}
 			</div>
 		);
 	}
