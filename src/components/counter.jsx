09854395080bcbd54c9773cc58a9249dc75c1e45
dgeorgiev <dgeorgiev_job@yahoo.com>
2020-03-08 17:03:55 +0200

    Updating the state counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,45 @@ import React, { Component } from "react";
 class Counter extends Component {
 	//special property
 	state = {
-		count: 0
+		count: 0,
+		tags: ["tag1", "tag2", "tag3"]
 	};
+
+	// constructor() {
+	// 	super();
+	// 	//functions are object and we have bind to set the value of 'this' to return new instcance
+	// 	//and reference current object
+	// 	this.handleIncrement = this.handleIncrement.bind(this);
+	// }
+
+	handleIncrement = () => {
+		console.log("Button clicked", this);
+		this.setState({ count: this.state.count + 1 });
+	};
+
+	renderTags() {
+		if (this.state.tags.length === 0) return <p>No more tags</p>;
+
+		return (
+			<ul>
+				{this.state.tags.map(tag => (
+					<li key={tag}>{tag}</li>
+				))}
+			</ul>
+		);
+	}
 
 	render() {
 		return (
 			<div>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-				<button className='btn btn-secondary btn-sm'>Increment</button>
+				<button
+					onClick={this.handleIncrement}
+					className='btn btn-secondary btn-sm'
+				>
+					Increment
+				</button>
+				{this.renderTags()}
 			</div>
 		);
 	}
